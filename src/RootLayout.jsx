a3b612033d2f4027fd5908e9fda8bd5816672dcb
ajhughesdev <andrew@ajhughes.dev>
2023-05-13 00:24:38 +0000

    server connection setup

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { useInView } from 'framer-motion'
+import AdSense from 'react-adsense'
 
 import Nav from './components/Nav/Nav'
 import Footer from './components/Footer/Footer'
@@ -30,7 +31,12 @@ const RootLayout = ({ darkMode, toggleDarkMode }) => {
       <div className={`Content ${isHome ? 'is-home' : ''}`} id='Content'>
         <Outlet />
       </div>
-
+      <AdSense.Google
+        client='ca-pub-7566894593607519'
+        slot='4080392724'
+        style={{ display: 'inline-block', width: '728px', height: '90px', margin: '1rem auto' }}
+        format=''
+      />
       <Footer />
     </div>
   )
